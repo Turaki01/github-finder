0bbba4206613e862c3192b9348cbb0f7c68ae4f2
Turaki01 <ezekiel_zakari@yahoo.com>
2019-12-25 03:06:39 +0100

    created a react app that consumes github API

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+const NavBar = ({ icon, title}) => {
+
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={icon}></i>
+                {title}
+            </h1>
+        </nav>
+    )
+
+};
+
+NavBar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fa fa-github'
+};
+
+NavBar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
+
+export default NavBar;
